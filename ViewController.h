3c890fa2e46b5fd58f2a6ae6894b1debf0ac925e
collett31 <charltoncollett1@gmail.com>
2019-09-12 00:38:53 -0500

    Add files via upload

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iCalculate
+//
+//  Created by Collett Charlton on 9/8/19.
+//  Copyright © 2019 Collett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UIGestureRecognizerDelegate, UITextFieldDelegate>
+
+
+@end
+
